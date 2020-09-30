f6e3e66775f4922092975b4ca618e8f1cf5a450a
Denver Fernandes <sub367@gmail.com>
2020-09-30 20:14:19 +0100

    Revert 1.a

--- parts/1/a/src/index.js
+++ parts/1/a/src/index.js
@@ -23,44 +23,43 @@ const Part = (props) => {
 
 const Total = ({parts}) => {
     const total = parts.reduce((total, current) => {
-        debugger
         return total+current.exercises
     }, 0);
     return <p>Number of exercises {total}</p>;
 };
 
 const Course = ({course}) => {
-  return(
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-  </div>)
+    return(
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>)
 };
 
 
 const App = () => {
-  const course = {
-      name: 'Half Stack application development',
-      parts: [
-          {
-              name: 'Fundamentals of React',
-              exercises: 10
-          },
-          {
-              name: 'Using props to pass data',
-              exercises: 7
-          },
-          {
-              name: 'State of a component',
-              exercises: 14
-          }
-      ]
-  };
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
-  return (
-    <Course course={course}/>
-  )
+    return (
+        <Course course={course}/>
+    )
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
