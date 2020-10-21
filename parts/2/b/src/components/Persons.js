fc7c89020b61c4c474b59cdbac680f7c07059731
Denver Fernandes <sub367@gmail.com>
2020-10-21 17:27:26 +0100

    2.c - load persons from local json server

--- parts/2/b/src/components/Persons.js
+++ parts/2/b/src/components/Persons.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Person = ({person}) => {
-    return <li key={person.name}>{person.name} {person.number}</li>;
+    debugger
+    return <li>{person.name} {person.number}</li>;
 };
 
 export const Persons = ({personsToShow}) => {
     return <ul>
-        {personsToShow.map(person => <Person person={person}/>)}
+        {personsToShow.map(person => <Person key={person.id} person={person}/>)}
     </ul>;
 };
