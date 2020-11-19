bc3e161c460807e362305a5c06d28d12130c43dc
Denver Fernandes <sub367@gmail.com>
2020-11-19 12:42:58 +0000

    Add part 3 exercise

---
+++ parts/3/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const argLength = process.argv.length;
+
+if (argLength < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0.2tryf.mongodb.net/test?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if (argLength < 5) {
+    console.log('phonebook:')
+    Person.find({}).then(persons => {
+        persons.forEach(person => console.log(person))
+        mongoose.connection.close()
+    })
+} else if (argLength === 5) {
+    const name = process.argv[3]
+    const number = process.argv[5]
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('Failed!')
+    process.exit(1)
+}
+
